21c5c978cf3a75af6cc8900c985a35f7522980ef
Anamarq <anamarq98@gmail.com>
2026-08-26 17:30:06 +0200

    add enemies to lever builder

--- Mistnoid/Assets/Scripts/Game/Levels/LevelBuilder.cs
+++ Mistnoid/Assets/Scripts/Game/Levels/LevelBuilder.cs
@@ -10,19 +10,65 @@ public class LevelBuilder : MonoBehaviour
     public void SaveLevel()
     {
         levelData.blocks.Clear();
+        levelData.enemyPositions.Clear();
+        levelData.enemyAcidPositions.Clear();
+
+        // blocks
 
         Block[] blocks = FindObjectsByType<Block>(FindObjectsSortMode.None);
+
         foreach (var b in blocks)
         {
-            Vector2Int gridPos = new Vector2Int(Mathf.RoundToInt(b.transform.position.x),Mathf.RoundToInt(b.transform.position.y));
+            Vector2Int gridPos = new Vector2Int(
+                Mathf.RoundToInt(b.transform.position.x),
+                Mathf.RoundToInt(b.transform.position.y)
+            );
+
             levelData.blocks.Add(new LevelBlock
             {
                 position = gridPos,
                 blockData = b.GetBlockData()
             });
         }
+
+        // enemies
+
+        Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+
+        foreach (var enemy in enemies)
+        {
+            Vector2Int gridPos = new Vector2Int(
+                Mathf.RoundToInt(enemy.transform.position.x),
+                Mathf.RoundToInt(enemy.transform.position.y)
+            );
+
+            levelData.enemyPositions.Add(gridPos);
+        }
+
+        // Acid enemies
+
+        AcidEnemy[] acidEnemies = FindObjectsByType<AcidEnemy>(FindObjectsSortMode.None);
+
+        foreach (var acidEnemy in acidEnemies)
+        {
+            Vector2Int gridPos = new Vector2Int(
+                Mathf.RoundToInt(acidEnemy.transform.position.x),
+                Mathf.RoundToInt(acidEnemy.transform.position.y)
+            );
+
+            levelData.enemyAcidPositions.Add(gridPos);
+        }
+
+        // save
         EditorUtility.SetDirty(levelData);
-        Debug.Log("Level saved!");
+        AssetDatabase.SaveAssets();
+
+        Debug.Log(
+            "Level saved! " +
+            blocks.Length + " bloques, " +
+            enemies.Length + " enemigos y " +
+            acidEnemies.Length + " enemigos de ácido."
+        );
     }
 }
 #endif
